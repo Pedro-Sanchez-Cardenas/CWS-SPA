81d225edbdae1de5801158e381cd32aab73d86e8
Pedro Sánchez Cárdenas <pedrosanchezcardenas@gmail.com>
2022-01-31 10:12:14 -0600

    Update php vertion

--- resources/views/content/plants/index.blade.php
+++ resources/views/content/plants/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentLayoutMaster')
 
-@section('title', 'Plans SWRO')
+@section('title', 'Water Treatment - RO')
 
 @section('vendor-style')
   <!-- vendor css files -->
@@ -12,8 +12,29 @@
 
 @section('content')
     <!-- Dashboard Analytics Start -->
-    <section id="dashboard-analytics">
-
+    <section id="plants" class="container">
+        <div class="row">
+            @foreach ($plants as $plant)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $plant->name }}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        </ul>
+                        <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </section>
     <!-- Dashboard Analytics end -->
 @endsection
