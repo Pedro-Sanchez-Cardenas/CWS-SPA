42b73f01da5b2ccad7ab7d338570e9cb7af33123
Eduardo Gabriel Cardenas Tzec <eduardo_1990cardenas@hotmail.com>
2022-02-15 10:59:38 -0600

    create view preview

--- resources/views/content/operations/plants/create.blade.php
+++ resources/views/content/operations/plants/create.blade.php
@@ -322,12 +322,12 @@
                                     <div class="input-group input-group-merge mb-2">
                                         <span class="input-group-text">
                                             <div class="form-check">
-                                                <input type="checkbox" name="botM3" class="form-check-input"
-                                                    id="colorRadio1">
+                                                <input type="radio" name="bot" class="form-check-input"
+                                                    id="botM3">
                                                 <label class="form-check-label" for="colorRadio1"></label>
                                             </div>
                                         </span>
-                                        <input type="number" name="" class="form-control" placeholder="0.00"
+                                        <input type="number" name="botM3" class="form-control" placeholder="0.00"
                                             aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">USD/M3</span>
                                     </div>
@@ -340,12 +340,12 @@
                                     <div class="input-group input-group-merge mb-2">
                                         <span class="input-group-text">
                                             <div class="form-check">
-                                                <input type="radio" name="botFixed" class="form-check-input"
-                                                    id="colorRadio1">
+                                                <input type="radio" name="bot" class="form-check-input"
+                                                    id="botFixed">
                                                 <label class="form-check-label" for="colorRadio1"></label>
                                             </div>
                                         </span>
-                                        <input type="text" name="" class="form-control" placeholder="0.00"
+                                        <input type="number" name="botFixed" class="form-control" placeholder="0.00"
                                             aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">USD/Month</span>
                                     </div>
@@ -358,11 +358,11 @@
                                     <div class="input-group input-group-merge mb-2">
                                         <span class="input-group-text">
                                             <div class="form-check">
-                                                <input type="radio" name="o&mM3" class="form-check-input" id="colorRadio1">
+                                                <input type="radio" name="o&m" class="form-check-input" id="o&mM3">
                                                 <label class="form-check-label" for="colorRadio1"></label>
                                             </div>
                                         </span>
-                                        <input type="text" name="" class="form-control" placeholder="0.00"
+                                        <input type="number" name="o&mM3" class="form-control" placeholder="0.00"
                                             aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">USD/M3</span>
                                     </div>
@@ -375,12 +375,12 @@
                                     <div class="input-group input-group-merge mb-2">
                                         <span class="input-group-text">
                                             <div class="form-check">
-                                                <input type="radio" name="o&mFixed" class="form-check-input"
-                                                    id="colorRadio1">
+                                                <input type="radio" name="o&m" class="form-check-input"
+                                                    id="o&mFixed">
                                                 <label class="form-check-label" for="colorRadio1"></label>
                                             </div>
                                         </span>
-                                        <input type="text" name="" class="form-control" placeholder="0.00"
+                                        <input type="number" name="o&mFixed" class="form-control" placeholder="0.00"
                                             aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">USD/Month</span>
                                     </div>
@@ -393,12 +393,12 @@
                                     <div class="input-group input-group-merge mb-2">
                                         <span class="input-group-text">
                                             <div class="form-check">
-                                                <input type="radio" class="form-check-input" name="remineralisationM3"
+                                                <input type="checkbox" class="form-check-input" name="remineralisationM3"
                                                     id="colorRadio1">
                                                 <label class="form-check-label" for="colorRadio1"></label>
                                             </div>
                                         </span>
-                                        <input type="text" name="" class="form-control" placeholder="0.00"
+                                        <input type="number" name="" class="form-control" placeholder="0.00"
                                             aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">USD/M3</span>
                                     </div>
@@ -498,7 +498,7 @@
                                                 <path
                                                     d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
                                             </svg></span>
-                                        <input type="text" class="form-control" placeholder="0.00"
+                                        <input type="number" class="form-control" placeholder="0.00"
                                             name="minimumConsumption" aria-label="Amount (to the nearest dollar)">
                                         <span class="input-group-text">M3/MONTH</span>
                                     </div>
@@ -562,7 +562,7 @@
                                                                         <path
                                                                             d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
                                                                     </svg></span>
-                                                                <input type="text" class="form-control"
+                                                                <input type="number" class="form-control"
                                                                     name="trainCapacity" placeholder="0.00"
                                                                     aria-label="Amount (to the nearest dollar)">
                                                             </div>
@@ -579,7 +579,7 @@
                                                                         <path
                                                                             d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
                                                                     </svg></span>
-                                                                <input type="text" name="trainTds" class="form-control"
+                                                                <input type="number" name="trainTds" class="form-control"
                                                                     placeholder="0.00"
                                                                     aria-label="Amount (to the nearest dollar)">
                                                                 <span class="input-group-text">ppm</span>
@@ -626,7 +626,7 @@
                                                                         <path
                                                                             d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z" />
                                                                     </svg></span>
-                                                                <input type="text" class="form-control" name="meabrane"
+                                                                <input type="number" class="form-control" name="meabrane"
                                                                     placeholder="0.00"
                                                                     aria-label="Amount (to the nearest dollar)">
                                                             </div>
