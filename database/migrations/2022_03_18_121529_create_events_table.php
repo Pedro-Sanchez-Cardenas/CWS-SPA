41707eaa2a0a4112a4061029b825805f4e826711
Eduardo Gabriel Cardenas Tzec <eduardo_1990cardenas@hotmail.com>
2022-03-18 12:48:22 -0600

    Update table event

---
+++ database/migrations/2022_03_18_121529_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('companies_id')->constrained();
+            $table->string('event');
+            $table->date('start_date');
+            $table->date('finish_date');
+            $table->foreignId('user_created_at')->constrained();
+            $table->foreignId('user_updated_at')->nullable()->constrained();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
