e9c9cad50b530f9a51883cb4d73645152284c546
Pedro Adrian Sanchez Cardenas <it@cwsmexico.mx>
2022-04-18 12:20:41 -0500

    Update search parameters in view parameters

--- app/Http/Livewire/Operation/Parameters/ViewParameters.php
+++ app/Http/Livewire/Operation/Parameters/ViewParameters.php
@@ -4,21 +4,47 @@ namespace App\Http\Livewire\Operation\Parameters;
 
 use App\Models\Plant;
 use Livewire\Component;
-use Livewire\WithPagination;
+use phpDocumentor\Reflection\Types\Null_;
 
 class ViewParameters extends Component
 {
 
     public Plant $plant;
+    public $date_range;
 
-    use WithPagination;
-
-    protected $paginationTheme = 'bootstrap';
+    protected $queryString = [
+        'date_range' => ['except' => ''],
+    ];
 
     public function render()
     {
+        $dates = explode(" ", $this->date_range);
         return view('livewire.operation.parameters.view-parameters', [
-            'plants' => Plant::where('id', $this->plant->id)->with('product_waters', 'pretreatments', 'operations')->get(),
+            'plants' => empty($this->date_range) ? (Plant::where('id', $this->plant->id)->with('product_waters', 'pretreatments', 'operations')->get()) : ((count($dates) > 2) ? $this->query() : Plant::where('id', $this->plant->id)->with('product_waters', 'pretreatments', 'operations')->get()),
         ]);
+    }
+
+    private function query()
+    {
+        $dates = explode(" ", $this->date_range);
+        if (count($dates) > 2) {
+            $plants = Plant::where('id', $this->plant->id)->with(
+                [
+                    'product_waters' => function ($query) {
+                        $dates = explode(" ", $this->date_range);
+                        $query->whereBetween('created_at', [$dates[0], $dates[2]]);
+                    },
+                    'pretreatments' => function ($query) {
+                        $dates = explode(" ", $this->date_range);
+                        $query->whereBetween('created_at', [$dates[0], $dates[2]]);
+                    },
+                    'operations' => function ($query) {
+                        $dates = explode(" ", $this->date_range);
+                        $query->whereBetween('created_at', [$dates[0], $dates[2]]);
+                    }
+                ]
+            )->get();
+            return $plants;
+        }
     }
 }
